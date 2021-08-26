280c0c1b4361780b203fb4335c77532e89936f5d
unknown <theja.pasupulati@gmail.com>
2021-08-26 07:21:58 +0530

    assinment_project

---
+++ assignment13_timefield/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for assignment13_timefield project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'assignment13_timefield.settings')
+
+application = get_wsgi_application()
